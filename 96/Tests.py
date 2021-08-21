35d750c3ec9d76552bc337871b8e5df9cfb03f1f
bbordwell <benbordwell@hotmail.com>
2021-08-21 15:21:45 -0500

    update Tests.py as well, missed in last commit

--- 96/Tests.py
+++ 96/Tests.py
@@ -5,7 +5,7 @@ from Euler96 import Board
 class Euler96TestCase(unittest.TestCase):
     """Tests for Euler96.py"""
 
-    def test_Board(self):
+    def test_Board_solve(self):
         """Can Board solve a board that requires only logic"""
         testBoard = Board([0, 0, 3, 0, 2, 0, 6, 0, 0, 9, 0, 0, 3, 0, 5, 0, 0, 1, 0, 0, 1, 8,
          0, 6, 4, 0, 0, 0, 0, 8, 1, 0, 2, 9, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 6, 7, 0,
@@ -15,6 +15,15 @@ class Euler96TestCase(unittest.TestCase):
                                           1, 8, 7, 6, 4, 9, 3, 5, 4, 8, 1, 3, 2, 9, 7, 6, 7, 2, 9, 5,
                                           6, 4, 1, 3, 8, 1, 3, 6, 7, 9, 8, 2, 4, 5, 3, 7, 2, 6, 8, 9, 
                                           5, 1, 4, 8, 1, 4, 2, 5, 3, 7, 6, 9, 6, 9, 5, 4, 1, 7, 3, 8, 2])
+
+    def test_Board_invalid_board(self):
+        "Gives Board an invalid starting board and tests that Board.validBoard gets set to False"
+        testBoard = Board([5, 0, 3, 0, 2, 0, 6, 0, 0, 9, 0, 0, 3, 0, 5, 0, 0, 1, 0, 0, 1, 8,
+         0, 6, 4, 0, 0, 0, 0, 8, 1, 0, 2, 9, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 6, 7, 0,
+          8, 2, 0, 0, 0, 0, 2, 6, 0, 9, 5, 0, 0, 8, 0, 0, 2, 0, 3, 0, 0, 9, 0, 0, 5, 0, 1, 0, 3, 0, 0])
+        testBoard.solveAttempt()
+        self.assertEqual(testBoard.validBoard,False)
+
 
 if __name__ == '__main__':
     unittest.main()
