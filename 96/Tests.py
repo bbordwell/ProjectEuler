c1d878aa8040fbc582d17f0615bc20c14431f375
bbordwell <benbordwell@hotmail.com>
2021-08-21 16:01:51 -0500

    Added a test for an input of an already complete puzzle

--- 96/Tests.py
+++ 96/Tests.py
@@ -33,5 +33,14 @@ class Euler96TestCase(unittest.TestCase):
         self.assertEqual(testBoard.validBoard,True)
         self.assertEqual(testBoard.solvedBoard,False)
 
+    def test_already_solved(self):
+        """Gives Board an already solved valid board and tests that it marks is as solved and valid"""
+        testBoard = Board([4, 8, 3, 9, 2, 1, 6, 5, 7, 9, 6, 7, 3, 4, 5, 8, 2, 1, 2, 5, 1, 8, 7, 6, 4,
+                           9, 3, 5, 4, 8, 1, 3, 2, 9, 7, 6, 7, 2, 9, 5, 6, 4, 1, 3, 8, 1, 3, 6, 7, 9,
+                           8, 2, 4, 5, 3, 7, 2, 6, 8, 9, 5, 1, 4, 8, 1, 4, 2, 5, 3, 7, 6, 9, 6, 9, 5, 4, 1, 7, 3, 8, 2])
+        testBoard.solveAttempt()
+        self.assertEqual(testBoard.solvedBoard,True)
+        self.assertEqual(testBoard.validBoard,True)
+
 if __name__ == '__main__':
     unittest.main()
