1b4a6c14b3cc1f3ed9061a004944ee4022e6bab7
bbordwell <benbordwell@hotmail.com>
2021-08-25 17:02:20 -0500

    Add a solution for Euler #22

---
+++ 22/22.py
@@ -0,0 +1,19 @@
+#This program solves Project Euler #22
+
+f = open('names.txt')
+names = f.read()
+names = names.replace('"','')
+names = names.split(',')
+names.sort()
+
+def score(name):
+    letters = "0ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ans = 0
+    for letter in name:
+        ans += letters.index(letter)
+    return ans
+
+ans = 0
+for index,name in enumerate(names):
+    ans += score(name) * (index+1)
+print(ans)
