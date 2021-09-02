10e61520bc74df3c00e376f31ea35242ba16e7b9
bbordwell <benbordwell@hotmail.com>
2021-09-02 01:21:00 -0500

    Add a solution for Euler #42

---
+++ 42/42.py
@@ -0,0 +1,25 @@
+import sys      #Do this so I can import usefulfuntions from parent directory. There must be a better way to do this?
+sys.path.append("/home/bbordwell/Documents/SRC/ProjectEuler")   #Edit this to fit your device
+
+from usefulfunctions import triangleNumbersGenerator
+
+def score(word):
+    """Given a name in all capital letters return its alphabetical value"""
+    letters = "0ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ans = 0
+    for letter in word:
+        ans += letters.index(letter)
+    return ans
+
+f = open("words.txt")
+words = f.read()
+words = words.split(',')
+words = [word.replace('"','') for word in words]
+
+triangles = set(triangleNumbersGenerator(limit=193))
+
+ans = 0
+for word in words:
+    if score(word) in triangles:
+        ans += 1
+print(ans)
