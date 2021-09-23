c979e216354598dd1f1c36c20bbbad818bc4a203
bbordwell <benbordwell@hotmail.com>
2021-09-23 18:29:51 -0500

    Add a solution for Euler #66

---
+++ 66/66.py
@@ -0,0 +1,39 @@
+from decimal import *
+getcontext().prec = 100
+
+#This program solves Project Euler #66
+
+def convergentsGenerator(d):
+    """Generate convergents of a given non perfect square int."""
+    r = Decimal(d).sqrt()
+    remainders = []
+    h,k,a = [0,1],[1,0],[]
+    while True:
+        a.append(int(r))
+        r -= a[-1]
+        remainders.append(float(r))
+        r = 1/r
+        h.append((a[-1]*h[-1])+h[-2])
+        k.append((a[-1]*k[-1])+k[-2])
+        yield (h[-1],k[-1])
+    
+
+def solvePells(d):
+    """Find the fundamental solution of for a Pell's equation given D."""
+    convergents = convergentsGenerator(d)
+    while True:
+        convergant = next(convergents)
+        if convergant[0]**2 - d*convergant[1]**2 == 1:
+            return convergant
+
+ds = [x for x in range(2,1_001) if x**.5 % 1 != 0]
+
+maxD = 0
+maxX = 0
+for d in ds:
+    solution = solvePells(d)
+    if solution[0] > maxX:
+        maxX = solution[0]
+        maxD = d
+print(maxD)
+    
