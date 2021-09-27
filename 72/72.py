5f62fd1dd67810182f647d870ec0b3fad335403f
bbordwell <benbordwell@hotmail.com>
2021-09-26 19:07:08 -0500

    Add a solution for Euler #72

---
+++ 72/72.py
@@ -0,0 +1,9 @@
+import sys      #Do this so I can import usefulfuntions from parent directory. There must be a better way to do this?
+sys.path.append("/home/bbordwell/Documents/SRC/ProjectEuler")   #Edit this to fit your device
+
+from usefulfunctions import phis
+
+#This program solves Project Euler #72.
+
+print(sum(phis(999_999).values()))
+#https://wikimedia.org/api/rest_v1/media/math/render/svg/a5946a2637b50c6d7d552808c2c7732216b91fff
